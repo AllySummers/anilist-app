abcd8d3bbeb4464b4df67d5779e7146142aaa910
Ally Summers <ally.summers@outlook.com.au>
2025-01-05 19:37:39 +1100

    chore(repo): Update eslint config to include more strict config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 import prettierConfig from 'eslint-plugin-prettier/recommended';
+import tseslint from 'typescript-eslint';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,69 @@ const compat = new FlatCompat({
 	baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript'), prettierConfig];
+const config = tseslint.config([
+	...compat.extends('next/core-web-vitals', 'next/typescript'),
+	prettierConfig,
+	{
+		rules: {
+			'import/order': [
+				'error',
+				{
+					groups: [
+						'builtin',
+						'external',
+						'internal',
+						'parent',
+						'sibling',
+						'index',
+						'object',
+					],
+					pathGroups: [
+						{
+							pattern: '@/**',
+							group: 'internal',
+							position: 'before',
+						},
+						{
+							pattern: 'react',
+							group: 'external',
+							position: 'before',
+						},
+						{
+							pattern: 'react-dom',
+							group: 'external',
+							position: 'before',
+						},
+						{
+							pattern: 'next',
+							group: 'external',
+							position: 'before',
+						},
+						{
+							pattern: 'next/**',
+							group: 'external',
+							position: 'before',
+						},
+					],
+					alphabetize: {
+						order: 'asc',
+					},
+				},
+			],
+		},
+	},
+	tseslint.configs.recommendedTypeChecked,
+	{
+		languageOptions: {
+			parser: tseslint.parser,
+			parserOptions: {
+				projectService: {
+					allowDefaultProject: ['*.js', '*.mjs'],
+				},
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
+]);
 
-export default eslintConfig;
+export default config;
